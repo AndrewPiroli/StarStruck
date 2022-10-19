2a7fdcdb663d5f9fa23813711375aa60ae2d455d
Andrew Piroli <drewpiroli@gmail.com>
2022-10-19 13:46:14 -0400

    Remove SDHC testing panic and print f_mount result

--- kernel/source/main.c
+++ kernel/source/main.c
@@ -107,9 +107,9 @@ void kernel_main( void )
 	
 	printk("Initializing SDHC...\n");
 	sdhc_init();
-	panic("REE");
 	printk("Mounting SD...\n");
 	fres = f_mount(0, &fatfs);
+	printk("Got %d from f_mount", fres);
 
 	if (read32(HW_CLOCKS) & 2) {
 		printk("GameCube compatibility mode detected...\n");
